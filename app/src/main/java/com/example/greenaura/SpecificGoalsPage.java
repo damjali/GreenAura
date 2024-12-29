3ba2ef764617792b75d6e21f944c1b62ef88701f
damjali <adamrazali54@gmail.com>
2024-12-30 04:48:25 +0800

    latest from adam

--- app/src/main/java/com/example/greenaura/SpecificGoalsPage.java
+++ app/src/main/java/com/example/greenaura/SpecificGoalsPage.java
@@ -19,29 +19,14 @@ private ActivitySpecificGoalsPageBinding binding;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivitySpecificGoalsPageBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        binding = ActivitySpecificGoalsPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_specific_goals_page);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_specific_goals_page);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-}
